335748a173e90178c9b99c68f3a00f7c6c8f90d4
RONAK JOSHI <51354961+rnkjoshi@users.noreply.github.com>
2020-05-23 12:51:26 +0530

    Add files via upload

---
+++ cpp/day08.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+float mean(int a,int b,int c,int d, int e,int f){
+    return ((float)(a+b+c+d+e+f)/6.0);
+}
+int main(){
+    cout<<mean(8,10,9,8,7,12);
+}
