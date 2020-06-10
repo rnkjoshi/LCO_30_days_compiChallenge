fc5b940200d1e3649379d825b1d0aec42eabe592
RONAK JOSHI <51354961+rnkjoshi@users.noreply.github.com>
2020-06-10 14:35:09 +0530

    Add files via upload

---
+++ cpp/day26.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int tempC;
+    cin>>tempC;
+    float tempF = (tempC*9/5)+32;
+    cout<<" Temperature in Fahrenheit = "<<tempF;
+}
