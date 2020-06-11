1614459bc853ce9e872255621cd2f05ee2635c1e
RONAK JOSHI <51354961+rnkjoshi@users.noreply.github.com>
2020-06-11 23:33:10 +0530

    Add files via upload

---
+++ cpp/day27.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double inch;
+    cin>>inch;
+    double feet = inch*0.0833;
+    double cm = inch*2.54;
+    double km = cm * 0.00001;
+    cout<<"feet = "<<feet<<"\ncm = "<<cm<<"\nkm = "<<km;
+}
