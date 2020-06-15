3a4e231ceaf2cda43059ff774ba993095fae243c
RONAK JOSHI <51354961+rnkjoshi@users.noreply.github.com>
2020-06-15 23:35:31 +0530

    Add files via upload

---
+++ cpp/day30.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    cout<<n+m<<endl;
+    cout<<n-m<<endl;
+    cout<<n*m<<endl;
+    cout<<n/m<<endl;
+    cout<<min(n,m)<<endl;
+    cout<<max(n,m)<<endl;
+}
